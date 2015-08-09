f45cf5e22875ddee8105cc5aeaf925ad0e22fb8f
isebmo <sebastien.mouret@gmail.com>
2015-08-09 22:03:50 +0200

    ajout beta, archives, hauteur

--- js/vendor/app.js
+++ js/vendor/app.js
@@ -2,9 +2,34 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-$('.content-post').readingTime();
+$('.post-content').readingTime();
 
 $(".menu-link").click(function(){
     $("#menu").toggleClass("active");
     $(".container").toggleClass("active");
+});
+
+$(document).ready(function() {
+    function extracted() {
+        maxHeight = Math.max.apply(
+            Math, boxes.map(function () {
+                return $(this).height();
+            }).get());
+        boxes.height(maxHeight);
+    }
+
+    $(window).load(function() {
+        boxes = $('.posts-list-img');
+        if (boxes) {
+            extracted();
+        }
+    });
+
+    $(window).resize(function () {
+        boxes = $('.posts-list-img');
+        if (boxes) {
+            boxes.height('auto');
+            extracted();
+        }
+    });
 });
